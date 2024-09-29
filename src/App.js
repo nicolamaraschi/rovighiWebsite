e62445b9b9e895f0c315ca1a8c5661a63c1c202a
nicolamaraschi <nicola.maraschi01@gmail.com>
2024-09-29 17:12:46 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -12,18 +12,22 @@ import "slick-carousel/slick/slick.css"; // Importa il CSS di slick-carousel
 import "slick-carousel/slick/slick-theme.css"; // Importa il tema di slick-carousel
 
 function App() {
+  // Usa il basename solo in produzione (ad esempio, su GitHub Pages)
+  const basename = process.env.NODE_ENV === 'production' ? '/rovighiWebsite' : '';
+
   return (
-    <Router>
+    <Router basename={basename}>
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Navigate to="/home" replace />} />  {/* Reindirizza alla route /home */}
+          <Route path="/home" element={<Home />} />  {/* Aggiungi la route /home */}
           <Route path="/servizi" element={<Servizi />} />
-      
           <Route path="/orologi" element={<Orologi />} />
-          
           <Route path="/contatti" element={<Contatti />} />
           <Route path="/ecommerce" element={<Ecommerce />} />  
+          {/* Reindirizzamento da qualsiasi percorso sconosciuto alla home */}
+          <Route path="*" element={<Navigate to="/home" replace />} />
         </Routes>
         <Footer />
       </div>
