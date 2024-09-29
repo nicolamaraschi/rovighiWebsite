77e0cf06fb5c10357fae9c1ff516533b1205da8f
nicolamaraschi <nicola.maraschi01@gmail.com>
2024-09-29 17:03:29 +0200

    possibile soluzione per basename

---
+++ README.md
@@ -0,0 +1,2 @@
+il comando per buildare il deploy su pages è: npm run deploy
+il comando per runnare su localhoste è: npm start
--- src/App.js
+++ src/App.js
@@ -12,16 +12,17 @@ import "slick-carousel/slick/slick.css"; // Importa il CSS di slick-carousel
 import "slick-carousel/slick/slick-theme.css"; // Importa il tema di slick-carousel
 
 function App() {
+  // Usa il basename solo in produzione (ad esempio, su GitHub Pages)
+  const basename = process.env.NODE_ENV === 'production' ? '/rovighiWebsite' : '';
+
   return (
-    <Router>
+    <Router basename={basename}>
       <div className="App">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/servizi" element={<Servizi />} />
-      
           <Route path="/orologi" element={<Orologi />} />
-          
           <Route path="/contatti" element={<Contatti />} />
           <Route path="/ecommerce" element={<Ecommerce />} />  
         </Routes>
